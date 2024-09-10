dc51437b807c9b15f5cdcc565e600f38990b8daa
rulez22 <138847805+rulez22@users.noreply.github.com>
2024-09-10 06:49:50 -0700

    adding redirect for netlify

--- src/components/Sign-In-Form/Sign-In-Form.component.jsx
+++ src/components/Sign-In-Form/Sign-In-Form.component.jsx
@@ -1,7 +1,7 @@
 import { useState} from 'react';
 import { 
           signInWithGooglePopup,
-          createUserDocumentFromAuth,
+         // createUserDocumentFromAuth,
           signInAuthUserWithEmailAndPassword
       } 
  from '../../utils/firebase/Firebase.utils';
